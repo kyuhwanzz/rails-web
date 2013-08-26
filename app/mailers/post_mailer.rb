4c8c3ef65c14adf7b8c4362a77107d3a91ceb2eb
Kyuhwan Yun <kyuhwanzz@gmail.com>
2013-08-26 10:07:55 +0000

    initialize repo.

---
+++ app/mailers/post_mailer.rb
@@ -0,0 +1,6 @@
+class PostMailer < ActionMailer::Base
+  default from: "msdn.isdg@google.com"
+
+  def send_email_notification(post)
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Forum::Application.config.secret_key_base = '676c3b1b63cc2f2c4b8657e7865f88899b687b9ad079726e5d062273efbda91d2f21e931cf66c07ca52b7bc6e3a83d2a036d7034d15266dc037b0415127240b4'
